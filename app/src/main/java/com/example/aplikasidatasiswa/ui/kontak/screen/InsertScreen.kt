e4e1569ffbe86fa2752c97fb5ea023e3c3ae549a
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2023-12-26 21:26:57 +0700

    add fun EntryKontakBody

--- app/src/main/java/com/example/aplikasidatasiswa/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/aplikasidatasiswa/ui/kontak/screen/InsertScreen.kt
@@ -4,14 +4,44 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.aplikasidatasiswa.ui.kontak.viewmodel.InsertUiEvent
+import com.example.aplikasidatasiswa.ui.kontak.viewmodel.InsertUiState
+import okhttp3.Challenge
+
+@Composable
+fun EntryKontakBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChallenge: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInputSiswa(
+            insertUiEvent = insertUiState.inserUiEvent,
+            onValueChange = onSiswaValueChallenge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
