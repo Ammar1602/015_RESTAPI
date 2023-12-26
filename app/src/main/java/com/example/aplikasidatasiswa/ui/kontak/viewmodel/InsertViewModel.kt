32129cff1eecd90a0787c38ea36814ebae9bd2f4
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2023-12-26 20:08:10 +0700

    add data class InsertUiState, masi eror mas soalnya tadi dah buat lupa di komit

---
+++ app/src/main/java/com/example/aplikasidatasiswa/ui/kontak/viewmodel/InsertViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.aplikasidatasiswa.ui.kontak.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.aplikasidatasiswa.model.Kontak
+import com.example.aplikasidatasiswa.repository.KontakRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){
+
+}
+
+data class InsertUiState(
+    val inserUiEvent: InsertUiEvent = InsertUiEvent(),
+)
+
